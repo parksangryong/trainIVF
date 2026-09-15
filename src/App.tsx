4af5b05b22d36e10983beaf80775d82ea9576ffb
박상룡 <ppsr3478@naver.com>
2026-09-15 16:03:30 +0900

    env set

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App({ url }: { url?: string }) {
   ];
   const router = url ? createMemoryRouter(routes, { initialEntries: [url] }) : createBrowserRouter(routes);
 
-  return <><RouterProvider router={router} /><AdSense slot={import.meta.env.VITE_ADSENSE_SLOT} /><a href="/privacy" style={{display:"block",textAlign:"center",margin:"0 0 28px",color:"#7898a9",fontSize:12}}>개인정보처리방침</a></>;
+  return <><RouterProvider router={router} /><AdSense slot={import.meta.env.VITE_ADS_DISPLAY_HORIZONTAL_FOOTER} /><a href="/privacy" style={{display:"block",textAlign:"center",margin:"0 0 28px",color:"#7898a9",fontSize:12}}>개인정보처리방침</a></>;
 }
 
 export default App;
